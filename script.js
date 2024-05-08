dce00a69cdec17f82e130f7cb4ceb076e29ba23d
wrfmartinez <wrmartinez709@gmail.com>
2024-05-08 17:52:44 -0400

    Complete exercises 2 and 3

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 const doubleNums = nums.map(num => {
     return num * 2;
 })
-console.log(doubleNums);
+// console.log(doubleNums);
+
+// Exercise 2: Array destructuring
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+const [firstPizzaTopping, secondPizzaTopping] = pizzaToppings;
+// console.log(firstPizzaTopping);
+// console.log(secondPizzaTopping);
+
+// Exercise 3: Destructuring objects
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+const { make, model } = car;
+// console.log(make);
+// console.log(model);
+
