f3444e90ff764a86617c8bcd067b0d21c080b67d
wrfmartinez <wrmartinez709@gmail.com>
2024-05-08 18:47:22 -0400

    Complete lab

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 // Use `.map()` to iterate over the following array
 // Create a new array where each value is multiplied by 2 and log the new array:
 
-// const nums = [ 13, 87, 2, 89, 12, 4, 90, 63 ];
-// const doubleNums = nums.map(num => {
-//     return num * 2;
-// })
+const nums = [ 13, 87, 2, 89, 12, 4, 90, 63 ];
+const doubleNums = nums.map(num => {
+    return num * 2;
+})
 
-// console.log(doubleNums);
+console.log(doubleNums);
 
 // Exercise 2: Array destructuring
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
@@ -93,21 +93,32 @@
 
 // Exercise 10: Boolean gates
 // Simulated language configuration (change this variable to test)
-const localLangConfig = null; // Change to 'es', 'fr', etc., or keep it null
+// const localLangConfig = null; // Change to 'es', 'fr', etc., or keep it null
 
-// Create a variable called LANG
-// Assign LANG the value of localLangConfig or 'en' as a default
-const LANG = localLangConfig || 'en';
+// // Create a variable called LANG
+// // Assign LANG the value of localLangConfig or 'en' as a default
+// const LANG = localLangConfig || 'en';
 
-// Log the result
-console.log('Language setting:', LANG);
+// // Log the result
+// console.log('Language setting:', LANG);
 
-// Simulated user theme preference (change this variable to test)
-const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
+// // Simulated user theme preference (change this variable to test)
+// const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
 
-// Create a variable called USER_THEME
-// Assign USER_THEME the value of userSavedTheme or 'light' as a default
-const USER_THEME = userSavedTheme || 'light';
+// // Create a variable called USER_THEME
+// // Assign USER_THEME the value of userSavedTheme or 'light' as a default
+// const USER_THEME = userSavedTheme || 'light';
 
-// Log the result
-console.log('User theme setting:', USER_THEME);
+// // Log the result
+// console.log('User theme setting:', USER_THEME);
+
+// Exercise 11: Optional chaining
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+// const adventurer = {
+//     name: 'Alice',
+//   };
+  
+//   let cat = adventurer.cat?.age;
+  
+//   console.log(cat);
